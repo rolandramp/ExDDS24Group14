ebfb37054f05daad4079eacbcb44ab1982c237e9
Roland Ramp <roland.ramp@gmail.com>
2025-01-01 22:13:07 +0100

    scrape question, answers, loyers and some meta data

--- src/main.py
+++ src/main.py
@@ -2,6 +2,25 @@ from avvo_downloader import AvvoDownloader
 import json
 from pathlib import Path
 
+
+def scrape_websites(start:int, end:int, urls: list):
+    for url in urls[start:end]:
+        (title, question, answers, lawyers, posted_times, answer_card_text) = loader.scrape_website(url)
+        result = {
+            'url': url,
+            'title': title,
+            'question': question,
+            'answers': answers,
+            'lawyers': lawyers,
+            'posted_times': [str(time) for time in posted_times],
+            'answer_card_text': answer_card_text
+        }
+        filename = f'results_n_{start}.json'
+        with open(data_path.joinpath(filename), 'w', encoding='utf-8') as json_file:
+            json.dump(result, json_file, ensure_ascii=False, indent=4)
+        start += 1
+
+
 if __name__ == '__main__':
     loader = AvvoDownloader()
 
@@ -17,26 +36,9 @@ if __name__ == '__main__':
         for url in data.get(key):
             q_n_a_urls.append(url)
 
-    results = []
-    # if scraping fails go on from here
-    start:int = 454
-    end:int = 500
-    for url in q_n_a_urls[start:end]:
-        (title, question, answers, lawyers, posted_times, answer_card_text) = loader.scrape_website(url)
-        result = {
-            'url': url,
-            'title': title,
-            'question': question,
-            'answers': answers,
-            'lawyers': lawyers,
-            'posted_times': [str(time) for time in posted_times],
-            'answer_card_text': answer_card_text
-        }
-        filename = f'results_n_{start}.json'
-        with open(data_path.joinpath(filename), 'w', encoding='utf-8') as json_file:
-            json.dump(result, json_file, ensure_ascii=False, indent=4)
-        start += 1
-        results.append(result)
+    print(f'urls to scrape {len(q_n_a_urls)}')
 
-    with open('results2.json', 'w', encoding='utf-8') as json_file:
-        json.dump(results, json_file, ensure_ascii=False, indent=4)
+    # if scraping fails go on from here (start end)
+    scrape_websites(454, 500, q_n_a_urls)
+
+
